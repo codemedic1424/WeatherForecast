59a7a38b9277e661c52a40432545e3d6095db333
codemedic1424 <sunnier.stopper.0x@icloud.com>
2024-12-22 20:11:59 -0500

    Initial Commit

---
+++ WeatherForecast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherForecast
+//
+//  Created by Christopher Wilshusen on 12/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherForecastTests/WeatherForecastTests.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherForecastTests.swift
+//  WeatherForecastTests
+//
+//  Created by Christopher Wilshusen on 12/22/24.
+//
+
+import Testing
+
+struct WeatherForecastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
