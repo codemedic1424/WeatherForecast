c60e3e0a0f9cb822e0ce956c9900407101ce398d
codemedic1424 <sunnier.stopper.0x@icloud.com>
2024-12-22 20:29:46 -0500

    Project Start - Initial Committ

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,14 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        HStack {
+            VStack {
+                Text("Mon")
+                Image(systemName: "sun.max.fill")
+                    .foregroundStyle(Color.yellow)
+                Text("High: 70")
+                Text("Low: 50")
+                
+            }
+            .padding()
+        }
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Mon")
+            Image(systemName: "sun.max.fill")
+                .foregroundStyle(Color.yellow)
+            Text("High: 70")
+            Text("Low: 50")
+            
         }
         .padding()
     }
+    
 }
 
 #Preview {
