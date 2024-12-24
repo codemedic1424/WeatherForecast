975542b53c5d91922c211658924f99b86701b467
codemedic1424 <sunnier.stopper.0x@icloud.com>
2024-12-24 01:15:45 -0500

    Code Clean up and additional properties added

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            DayForecast(day: "Mon")
-            
-            DayForecast(day: "Tues")
-                
+        Text("Weekly Weather")
+            .font(.largeTitle)
+            .underline(color: .black)
+        VStack {
+            HStack {
+                DayForecast(day: "Mon", high: 72, low: 60, isRainy: false)
+                DayForecast(day: "Tues", high: 58, low: 30, isRainy: true)
+                DayForecast(day: "Wed", high: 56, low: 32, isRainy: false)
             }
             .padding()
+            HStack {
+                DayForecast(day: "Thurs", high: 76, low: 58, isRainy: false)
+                DayForecast(day: "Fri", high: 72, low: 50, isRainy: true)
+                DayForecast(day: "Sat", high: 60, low: 38, isRainy: false)
+                }
+            .padding()
+            HStack {
+                DayForecast(day: "Sun", high: 54, low: 30, isRainy: false)
+            }
+            .padding()
+        }
         }
     }
 
@@ -26,15 +40,41 @@ struct ContentView: View {
 
 struct DayForecast: View {
     let day: String
+    let high: Int
+    let low: Int
+    let isRainy: Bool
+    
+    var iconeName: String {
+        if isRainy {
+            return "cloud.rain.fill"
+        } else {
+            return "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            return Color.blue
+        } else {
+            return Color.red
+        }
+    }
+    
     var body: some View {
         VStack {
-            Text("Mon")
-            Image(systemName: "sun.max.fill")
-                .foregroundStyle(Color.yellow)
-            Text("High: 70")
-            Text("Low: 50")
-            
+            Text(day)
+                .font(.headline)
+            Image(systemName: iconeName)
+                .foregroundStyle(iconColor)
+            Text("High: \(high)º")
+                .fontWeight(.light)
+            Text("Low: \(low)º")
+                .fontWeight(.light)
+                .foregroundStyle(Color.secondary)
         }
+        .backgroundStyle(Color.teal)
         .padding()
+        .background(Color.blue).opacity((0.7))
+       
     }
 }
